933afce5ce78033968c78b0a75fb3bd7ee7a2e34
ElementalAang <49436592+ElementalAang@users.noreply.github.com>
2019-04-23 15:29:47 +0300

    Delete FullGroupException.java

--- Homework/FullGroupException.java
+++
@@ -1,8 +0,0 @@
-package Lesson4.Homework;
-
-public class FullGroupException extends Exception {
-    @Override
-    public String getMessage(){
-        return "Error. Cannot add student because the group is full";
-    }
-}
